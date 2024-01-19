9ca76b4e923186fff3b8e9622887a971ae860c92
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-01-19 22:25:20 +0100

    -a

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const middlewares = require('./middlewares');
+const dotenv = require('dotenv');
+
+
+
+dotenv.config();
+
+
+const app = express();
+const PORT = 4000;
+
+
+
+
+
+
+
+  app.use(session({
+    secret: process.env.PALABRA_SECRETA || 'secretoSuperSecreto',
+    resave: false,
+    saveUninitialized: true,
+  }));
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  routes.setup(app);
+  middlewares.setupApp(app);
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+  });
---
+++ middlewares.js
@@ -0,0 +1,45 @@
+const bodyParser = require('body-parser');
+
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+const validarPalabraMiddleware = (req, res, next) => {
+  const palabraCorrecta = process.env.PALABRA_SECRETA || '';
+
+  if (req.body.palabra === palabraCorrecta) {
+    req.session.palabraSecreta = req.body.palabra;
+    next();
+  } else {
+    res.redirect('/?error=1');
+  }
+};
+
+
+const setupAPP = (app) => {
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(session({
+    secret: 'secretoSuperSecreto',
+    resave: false,
+    saveUninitialized: true,
+  }));
+};
+
+const verificarSesionMiddleware = (req, res, next) => {
+  if (req.session.palabraSecreta) {
+    next();
+  } else {
+    res.redirect('/?error=2');
+  }
+};
+
+
+
+
+module.exports = {
+    validarPalabraMiddleware,
+    verificarSesionMiddleware,
+    setupAPP,
+  };
