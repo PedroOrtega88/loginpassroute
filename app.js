62ed90a3af2664d2bc3bda02bedf855019cf61d5
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-01-22 18:38:46 +0100

    -a

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
 const express = require('express');
-
-
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const middlewares = require('./middlewares');
 const dotenv = require('dotenv');
 
-
-
 dotenv.config();
-
 
 const app = express();
 const PORT = 4000;
 
-
-
-
-
-
-
-  app.use(session({
+app.use(session({
     secret: process.env.PALABRA_SECRETA || 'secretoSuperSecreto',
     resave: false,
     saveUninitialized: true,
-  }));
+}));
 
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  routes.setup(app);
-  middlewares.setupApp(app);
-
+routes.setup(app);
+middlewares.setupAPP(app);
 
 app.listen(PORT, () => {
     console.log(`Servidor en ejecución en http://localhost:${PORT}`);
-  });
+});
